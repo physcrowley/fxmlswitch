6df7067d165a5aef9366dd10b4213bf8a97f6c5a
physcrowley <crowlda@ecolecatholique.ca>
2021-01-21 11:26:07 -0500

    updated comments in App

--- fxmlswitch/src/main/java/edu/djc/fxmlswitch/App.java
+++ fxmlswitch/src/main/java/edu/djc/fxmlswitch/App.java
@@ -31,9 +31,19 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
     	scene = new Scene(loadFXML("primary"), 640, 480);
-    	scene.getStylesheets().add("app.css");
-    	//scene.getStylesheets().add(App.class.getResource("app.css").toExternalForm());
-    	//scene.getStylesheets().add(this.getClass().getResource("app.css").toExternalForm());
+    	
+    	//we placed the app.css file in the same output folder as App.class
+    	
+    	// doesn't work because Java cannot resolve the complete file path
+    	//scene.getStylesheets().add("app.css");
+    	//System.out.println("app.css");
+    	
+    	// works using the current class as a reference point and 
+    	// .toExternalForm() to generate a valid file path
+    	scene.getStylesheets().add(this.getClass().getResource("app.css").toExternalForm());
+    	//System.out.println(this.getClass());
+    	//System.out.println(this.getClass().getResource("app.css").toExternalForm());
+    	
     	stage.setScene(scene);
         stage.show();
     }
@@ -45,7 +55,7 @@ public class App extends Application {
     // Q : why we need this method instead of just calling
     // 		setRoot(Parent root) directly on scene in the start() method
     // A : because this method is called statically (using the class name instead
-    //		of using an objet) in both PrimaryController and SecondaryController
+    //		of using an object) in both PrimaryController and SecondaryController
     
     
     private static Parent loadFXML(String fxml) throws IOException {
@@ -60,6 +70,12 @@ public class App extends Application {
     // A: because the code is used in 2 different spots : start() and setRoot()
     //		and a method prevents copying and pasting code that needs to be
     //		reused in multiple places
+    
+    // Q: why can't we replace App.class with the more general this.getClass() in
+    //		this context?
+    // A: the `static` identifier means that the `loadFXML` method can only refer
+    //		to the class itself (the definitions here) and not an object created
+    //		by the class (an object can be referred to using `this.`)
 
     public static void main(String[] args) {
         launch();
